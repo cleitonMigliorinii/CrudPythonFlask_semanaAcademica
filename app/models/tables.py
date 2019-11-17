53ef1dbb480b1c328600d6da0a20d36d336c6403
Cleiton Migliorini <cleitonmigliorinii@gmail.com>
2019-11-17 17:19:18 +0000

    Ajustes

---
+++ app/models/tables.py
@@ -0,0 +1,54 @@
+from app import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String)
+    name = db.Column(db.String)
+
+    def __init__(self, username, email,password,name):
+        self.username = username
+        self.email = email
+        self.password = password
+        self.name = name
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey='users.id')
+
+    user = db.relationship('User',foreign_key=user_id)
+
+
+    def __init__(self, content, user_id):
+        self.content = content
+        self.user_id = user_id
+
+    def __repr__(self):
+        return '<Post %r>' % self.id
+
+
+class Follow(db.Model):
+    __tablename__ = 'follows'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey='users.id')
+    follow_id = db.Column(db.Integer, db.ForeignKey='users.id')
+
+    user = db.relationship('User',foreign_key=user_id)
+    follow = db.relationship('User',foreign_key=follow_id)
+
+    def __init__(self, follow_id, user_id):
+        self.follow_id = follow_id
+        self.user_id = user_id
+
+    def __repr__(self):
+        return '<Post %r>' % self.id
